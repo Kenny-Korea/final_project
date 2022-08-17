d6671a166693f77c3f4de12f596f8e28144a9936
김광민 <kenny@gimgwangmin-ui-MacBookAir.local>
2022-08-17 15:12:13 +0900

    220817 second commit

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,11 +1,16 @@
 import { useState, React } from "react";
+import "../css(react)/course.css";
 
 const Course = () => {
-  let [course, setCourse] = useState([1, 2, 3, 4, 5]);
+  let [course, setCourse] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
   return (
     <>
-      <div>dd</div>
+      <div className="test">
+        {course.map((a, i) => {
+          return <div className="testDiv">{a}</div>;
+        })}
+      </div>
     </>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import "../css(react)/header";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -12,8 +13,8 @@ const Header = () => {
       {/* Navbar부분 */}
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Hello Class</Navbar.Brand>
-          <Nav className="me-auto">
+          <Navbar.Brand href="/">ONECLE</Navbar.Brand>
+          <Nav className="justify-content-start">
             <Nav.Link
               onClick={() => {
                 navigate("/course");
@@ -49,20 +50,25 @@ const Header = () => {
             >
               마이페이지
             </Nav.Link>
-            <Nav.Link
-              onClick={() => {
-                navigate("/login");
-              }}
+            <div
+              className="loginButton"
+              style={{ display: "flex", justifyContent: "flex-end" }}
             >
-              로그인
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => {
-                navigate("/mypage");
-              }}
-            >
-              마이페이지
-            </Nav.Link>
+              <Nav.Link
+                onClick={() => {
+                  navigate("/login");
+                }}
+              >
+                로그인
+              </Nav.Link>
+              <Nav.Link
+                onClick={() => {
+                  navigate("/mypage");
+                }}
+              >
+                마이페이지
+              </Nav.Link>
+            </div>
           </Nav>
         </Container>
       </Navbar>
